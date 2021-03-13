371a2086bbf2a42767cd4f72d5f8691f916d2ba1
o.ashouri <omidashouri@gmail.com>
2021-03-13 14:31:13 +0330

    Initial commit

---
+++ src/test/java/ir/omidashouri/springbatchone/SpringbatchoneApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.omidashouri.springbatchone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
